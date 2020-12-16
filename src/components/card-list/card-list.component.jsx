2ea5fcd65750cac3e85c25f511ef881c10ca917f
weiweisteve <niweiweish@gmail.com>
2020-12-16 16:00:12 -0700

    monsters-rolodex project

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './card-list.styles.css';
+import { Card } from '../card/card.component.jsx';
+
+export const CardList = props => {
+    // console.log(props);
+    // console.log(props.monsters);
+    return (
+        <div className='card-list'>
+            {props.monsters.map(monster => (
+                <Card key={monster.id} monster={monster} />
+            ))}
+        </div>
+    )
+}
